a70a5fa56304a74c9814cc3a57dcef569d798a7f
Kat Perreira <kat.perreira@gmail.com>
2018-08-28 20:50:26 -0700

    Added ArgError for invalid card types

--- lib/card.rb
+++ lib/card.rb
@@ -1,5 +1,6 @@
 
 # card.rb
+require 'pry'
 
 class Card
   attr_reader :value, :suit
@@ -7,9 +8,27 @@ class Card
   def initialize(value, suit)
     @value = value
     @suit = suit
-    
+    # Valid values
+    good_values_ints = *(1..13)
+    face_cards = ["Queen", "King", "Jack", "Ace"]
+    good_values = (good_values_ints << face_cards).flatten!
 
+    value_array = [@value]
+    value_comparisons = good_values - value_array
+    # binding.pry
+    if value_comparisons.length == 17
+      raise ArgumentError.new("Not a valid value")
+    end
+    # Valid suits
+    good_suits = [:diamonds, :hearts, :clubs, :spades]
+    suit_array = [@suit]
+    suit_comparisons = good_suits - suit_array
+    if suit_comparisons.length == 4
+      raise ArgumentError.new("Not a valid suit")
+    end
+    # binding.pry
   end
+
 
   def to_s
     return "#{value} of #{suit.to_s}"
